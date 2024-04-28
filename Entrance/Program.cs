aeec4f4c48b9df553258c8c180842b5ad4f3a22e
ximengcs <ximengcs@163.com>
2024-04-28 23:57:17 +0800

    update

---
+++ Entrance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace XFrameServer.Core
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
