d3580d90556bb46aac9f5efae1500b3a42b27ff8
ximengcs <simon.xi@buttontech.com>
2024-05-29 17:37:59 +0800

    update

--- Test/GameConst.cs
+++ Test/GameConst.cs
@@ -5,12 +5,14 @@ namespace XFrameServer.Test
 {
     public static class GameConst
     {
+        private const int NET_FIBER = 1;
+
         private static Fiber s_NetFiber;
         public static Fiber NetFiber => s_NetFiber;
 
         public static void Initialize()
         {
-            s_NetFiber = Global.Fiber.GetOrNew(1);
+            s_NetFiber = Global.Fiber.GetOrNew(NET_FIBER);
             s_NetFiber.StartThread(1);
             Global.Net.SetFiber(s_NetFiber);
         }
