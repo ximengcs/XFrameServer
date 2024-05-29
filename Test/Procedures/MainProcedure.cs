8915209f5879671bd1f11424cd503554b56ca71c
ximengcs <simon.xi@buttontech.com>
2024-05-29 18:32:48 +0800

    update

--- Test/Procedures/MainProcedure.cs
+++ Test/Procedures/MainProcedure.cs
@@ -28,14 +28,19 @@ namespace XFrameServer.Core.Procedures
             base.OnEnter();
 
             GameConst.Initialize();
+            InnerCreateServer();
+        }
 
+        #region Server
+        private void InnerCreateServer()
+        {
             Fiber serverFiber = Global.Fiber.GetOrNew(GameConst.FIBER_ID);
             serverFiber.StartThread();
             IScene serverScene = Global.Scene.Create(serverFiber);
-            serverScene.Fiber.Post(InnerCreateServer, serverScene);
+            serverScene.Fiber.Post(InnerCreateServerScene, serverScene);
         }
 
-        private void InnerCreateServer(object state)
+        private void InnerCreateServerScene(object state)
         {
             IScene serverScene = state as IScene;
             if (string.IsNullOrEmpty(Init.Options.Host))
@@ -46,5 +51,28 @@ namespace XFrameServer.Core.Procedures
                     Global.Net.Create(serverScene, NetMode.Server, ipAddress, 9999, XProtoType.Tcp);
             }
         }
+        #endregion
+
+        #region Client
+        private void InnerCreateClient()
+        {
+            Fiber clientFiber = Global.Fiber.GetOrNew(GameConst.FIBER_ID);
+            clientFiber.StartThread();
+            IScene clientScene = Global.Scene.Create(clientFiber);
+            clientScene.Fiber.Post(InnerCreateClientScene, clientScene);
+        }
+
+        private void InnerCreateClientScene(object state)
+        {
+            IScene clientScene = state as IScene;
+            if (string.IsNullOrEmpty(Init.Options.Host))
+                Global.Net.Create(clientScene, NetMode.Client, 9999, XProtoType.Tcp);
+            else
+            {
+                if (IPAddress.TryParse(Init.Options.Host, out IPAddress ipAddress))
+                    Global.Net.Create(clientScene, NetMode.Client, ipAddress, 9999, XProtoType.Tcp);
+            }
+        }
+        #endregion
     }
 }
